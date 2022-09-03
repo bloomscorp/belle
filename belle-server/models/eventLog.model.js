d8fa46c0daa3f73cf45b4f56e7b963f32e2c329e
Ayan Kumar Saha <ayankumarsaha96@gmail.com>
2022-09-03 13:34:58 +0530

    update event log model

--- belle-server/models/eventLog.model.js
+++ belle-server/models/eventLog.model.js
@@ -1,37 +1,48 @@
 const mongoose = require('mongoose');
-const {getUniqueId} = require('../utils/utils');
+// const { getUniqueId } = require('../utils/utils');
+const ConstantsService = require('../services/constant.service');
+const autoIncrement = require('./sequence.model');
 
 const EventLogSchema = new mongoose.Schema({
-    eventLogId: {
-        type: String,
-        default: getUniqueId(),
-        index: {unique: true}
-    },
-    eventId: {
-        type: String,
-        required: true
-    },
-    propertyId: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    ipAddress: {
-        type: String,
-    },
-    os: {
-        type: String,
-    },
-    basicSystemSettings: {
-        type: String,
-    },
-    data: {
-        type: mongoose.Schema.Types.Mixed
-    }
+	eventLogId: {
+		type: Number,
+		// default: getUniqueId(),
+		index: { unique: true },
+	},
+	eventId: {
+		type: Number,
+		required: true,
+	},
+	propertyId: {
+		type: Number,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		required: true,
+	},
+	ipAddress: {
+		type: String,
+	},
+	os: {
+		type: String,
+	},
+	basicSystemSettings: {
+		type: mongoose.Schema.Types.Mixed,
+	},
+	data: {
+		type: mongoose.Schema.Types.Mixed,
+	},
+});
+
+EventLogSchema.pre('save', function (next) {
+	if (!this.isNew) {
+		next();
+		return;
+	}
+
+	autoIncrement(ConstantsService.EVENT_LOG_SEQ_ID, this, next);
 });
 
 module.exports = mongoose.model('EventLog', EventLogSchema);
--- belle-server/services/constant.service.js
+++ belle-server/services/constant.service.js
@@ -3,6 +3,7 @@ class ConstantsService {
     // sequence Ids
     static PROPERTY_SEQ_ID = 'propertyId';
     static EVENT_SEQ_ID = 'eventId';
+    static EVENT_LOG_SEQ_ID = 'eventLogId';
 }
 
 module.exports = ConstantsService;
