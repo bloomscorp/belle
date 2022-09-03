cff384feeb6e3d1fed62087a1aad6d04a05f1180
Ayan Kumar Saha <ayankumarsaha96@gmail.com>
2022-09-03 12:27:39 +0530

    add auto incremental propertyId

--- belle-server/models/property.model.js
+++ belle-server/models/property.model.js
@@ -1,36 +1,42 @@
 const mongoose = require('mongoose');
-const { getUniqueId } = require('../utils/utils');
+// const { getUniqueId } = require('../utils/utils');
+const ConstantsService = require('../services/constant.service');
+const autoIncrement = require('./sequence.model');
 
-const PropertySchema = new mongoose.Schema({
-    propertyId: {
-        type: String,
-        default: getUniqueId(),
-        index: { unique: true }
-    },
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        index: { unique: true }
-    },
-    url: {
-        type: String,
-        required: true,
-        trim: true,
-        index: { unique: true }
-    },
-    userId: {
-        type: String,
-        required: true
-    }, 
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    }
+const PropertySchema = new mongoose.Schema(
+	{
+		propertyId: {
+			type: Number,
+			// default: getUniqueId(),
+			index: { unique: true },
+		},
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		url: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		userId: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+PropertySchema.pre('save', function (next) {
+	if (!this.isNew) {
+		next();
+		return;
+	}
+
+	autoIncrement(ConstantsService.PROPERTY_SEQ_ID, this, next);
 });
 
 module.exports = mongoose.model('Property', PropertySchema);
